9901dc4fde309911f23e82dd8fc02d18c42a737e
SemprebomTecnologia <matheusfreire.ti@hotmail.com>
2021-04-20 01:06:38 -0300

    Update home.blade.php

--- semprebomtecnologia/resources/views/home.blade.php
+++ semprebomtecnologia/resources/views/home.blade.php
@@ -2,3 +2,11 @@
 
 @section('title', 'sadsdsadsadsadsadsadsadsa')
 @section('content', 'funcionou o conteudo do curso , so mudar aqui o  que quero')
+@section('content', 'funcionou o conteudo do curso , so mudar aqui o  que quero')
+@section('content', 'funcionou o conteudo do curso , so mudar aqui o  que quero')
+@section('content', 'funcionou o conteudo do curso , so mudar aqui o  que quero')
+@section('content', 'funcionou o conteudo do curso , so mudar aqui o  que quero')
+@section('content', 'funcionou o conteudo do curso , so mudar aqui o  que quero')
+@section('content', 'funcionou o conteudo do curso , so mudar aqui o  que quero')
+@section('content', 'funcionou o conteudo do curso , so mudar aqui o  que quero')
+@section('content', 'funcionou o conteudo do curso , so mudar aqui o  que quero')
