b49713f0e9b037e63bc29c66a4b8dbbaff9bb4dd
SemprebomTecnologia <matheusfreire.ti@hotmail.com>
2021-04-30 18:18:05 -0300

    templete home
    home
    navbar
    footer

---
+++ camargoadvocacia/resources/views/layouts/footer.blade.php
@@ -0,0 +1,50 @@
+<div class="footer-dark">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3 col-md-3 item">
+                    <h3>Serviços</h3>
+                    <ul>
+                        <li><a href="servicosWEB">Web design</a></li>
+                        <li><a href="development">Development</a></li>
+                        <li><a href="hosting">Hosting</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 col-md-3 item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">Company</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 item text">
+                    <h3>Frase motivacional </h3>
+
+                    <div class="container">
+                        <div class="scroller">
+                            <div class="inner">
+                                <p>A solução na medida certa para seu negocio</p>
+                                <p>I record them in my home studio.</p>
+                                <p>But my real goal is to <a class="blue">make</a> <a class="blueb">music</a>.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                           
+
+                <div class="col-sm-3">
+                    <h4>Follow Us</h4>
+                    <ul class="social-network social-circle">
+                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#" class="icoyoutube"  title="youtube"><i class="fa fa-youtube"></i></a></li>
+                    </ul>				
+                </div>
+            </div>
+        </div>
+        
+        <p class="copyright"> Copyright © SemprebomTecnologia</p>        
+    </footer>
+</div>
---
+++ camargoadvocacia/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,41 @@
+<div>
+    <a class="navbar-brand">
+        <img src="imgs\Camargo-fundo-transparente.png" class="rounded mx-auto d-block bg-white "
+            alt="CAMARGO ADVOCACIA">
+    </a>
+</div>
+
+<header class="site-header sticky-top py-1  ">
+    <nav class="navbar navbar-expand-lg  justify-content-md-center  navbar-white bg-white ">
+        <!-- Image and text -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-md-center" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto ">
+                <li class="nav-item active">
+                    <a class="nav-link" href="home">Home <span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">&Aacute;reas de Atua&ccedil;&atilde;o</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Escritorio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Artigos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Agende sua reuni&atilde;o</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contato</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+</header>
+
